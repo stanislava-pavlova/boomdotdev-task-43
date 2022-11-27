f530179432d64cb36697b5abe4f571a56b7be2b9
Stanislava Pavlova <stanislavap2002@gmail.com>
2022-11-27 21:53:31 +0200

    added feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
-import "../scss/app.scss";
-
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   // This block will be executed once the page is loaded and ready
+  function updateTitle() {
+    setTimeout(() => {
+      document.title = 'One new message';
+      console.log('works');
+    }, 3000);
+  }
+  updateTitle();
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  const button = document.querySelector('.button');
+  button.addEventListener('click', () => {
+    alert('💣');
   });
 });
